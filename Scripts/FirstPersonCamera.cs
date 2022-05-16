a2d843dd48cdcb9aab493fb5253404e676291215
Westknight <samemy1@hotmail.com>
2022-05-16 08:32:43 -0500

    test2

---
+++ Scripts/FirstPersonCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    public float sensibility = 100;
+    float verticalRotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // barre le cursor et le fait disparaite
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 delta = Mouse.current.delta.ReadValue() * Time.deltaTime * sensibility;
+        verticalRotation -= delta.y;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        //player.Rotate(new Vector3(0, delta.x, 0));
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        player.Rotate(Vector3.up * delta.x); // Écriture beaucoup plus lisible
+    }
+}
---
+++ Scripts/mousePos.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class mousePos : MonoBehaviour
+{
+    [SerializeField] float mousePosition;
+    Camera camera;
+    Mouse mouse;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
