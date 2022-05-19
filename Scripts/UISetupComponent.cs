18449fe4a1dae0fbff85ecf369f918c1cf17276d
Westknight <samemy1@hotmail.com>
2022-05-19 17:06:44 -0500

    Crosshair Dynamic
    le crosshair est celui choisi dans le MainMenu setting, il change ingame d'apres le choix

--- Scripts/UISetupComponent.cs
+++ Scripts/UISetupComponent.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+//Avec l'aide de : https://answers.unity.com/questions/1331550/how-to-load-sprite-dynamically-from-assets-in-unit.html
 
 public class UISetupComponent : MonoBehaviour
 {
-    void Start()
+    [SerializeField] Image UiCrosshair;
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        string crosshairName = !string.IsNullOrEmpty(PlayerPrefs.GetString("Crosshair")) ? PlayerPrefs.GetString("Crosshair") : "Crosshair_0"; //Met comme default Crosshair_0
+        UiCrosshair.sprite = Resources.Load<Sprite>("Spites/"+crosshairName);
     }
 }
