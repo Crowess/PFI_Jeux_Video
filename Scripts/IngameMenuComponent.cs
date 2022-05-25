fd825c94f237eb9248f1262de9c3d1ac2ec7f1f3
Westknight <samemy1@hotmail.com>
2022-05-24 21:41:01 -0500

    Update IngameMenuComponent.cs

--- Scripts/IngameMenuComponent.cs
+++ Scripts/IngameMenuComponent.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class IngameMenuComponent : MonoBehaviour
 {
     [SerializeField] Canvas menu;
-    [SerializeField] FirstPersonCamera fpCamera;
+    FirstPersonCamera fpCamera;
     PlayerControls playerControls;
     void Awake()
     {
         playerControls = new PlayerControls();
         playerControls.PC.Menu.Enable();
         playerControls.PC.Menu.performed += Menu_performed;
+        fpCamera = GetComponent<FirstPersonCamera>();
     }
 
     private void Menu_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -20,11 +21,13 @@ public class IngameMenuComponent : MonoBehaviour
         {
             menu.gameObject.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
+            fpCamera.enabled = true;
         }
         else
         {
             menu.gameObject.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
+            fpCamera.enabled = false;
         }
             
     }
